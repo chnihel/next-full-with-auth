3d64aaa8d604535c96754c748e5ac4d68c62ca69
chnihel <cherif.nihel456@gmail.com>
2025-01-03 08:58:39 +0100

    initial commit

--- src/app/api/commande/route.ts
+++ src/app/api/commande/route.ts
@@ -80,62 +80,6 @@ return NextResponse.json({
   
 } 
 
-export async function GETBYID(req: Request) {
-  console.log("Requête reçue pour récupérer une commande par ID");
-
-
-  try {
-    
-  const { searchParams } = new URL(req.url);
-  const id = searchParams.get("id");
-  
-  console.log("ID reçu :", id); 
-
-  if (!id) {
-    return NextResponse.json(
-      { message: "ID manquant dans la requête", data: {} },
-      { status: 400 }
-    );
-  }
-    const collection1 = await connectDataBase();
-       if (!ObjectId.isValid(id)) {
-      return NextResponse.json(
-        { message: "ID invalide", data: {} },
-        { status: 400 }
-      );
-    }
-    const dataCommande = await collection1.findOne({ _id: new ObjectId(id) });
-
-    console.log("Commande trouvée :", dataCommande); 
-
-    if (dataCommande) {
-      return NextResponse.json(
-        {
-          message: "Commande récupérée avec succès",
-          dataCommande,
-        },
-        { status: 201 }
-      );
-    } else {
-      return NextResponse.json(
-        {
-          message: "Aucune commande trouvée avec cet ID",
-          data: {},
-        },
-        { status: 404 }
-      );
-    }
-  } catch (error) {
-    console.error("Erreur lors de la récupération de la commande", error);
-    return NextResponse.json(
-      {
-        message: "Erreur interne du serveur",
-        data: {},
-      },
-      { status: 500 }
-    );
-  }
-}
 
 export async function PUT(req: Request) {
   const { searchParams } = new URL(req.url);
