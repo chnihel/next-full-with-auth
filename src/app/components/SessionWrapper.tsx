c6974974f0b0505d5d3ed79a6c97598b6343eb7a
chnihel <cherif.nihel456@gmail.com>
2025-01-15 13:41:56 +0100

    second commit

--- src/app/components/SessionWrapper.tsx
+++ src/app/components/SessionWrapper.tsx
@@ -2,8 +2,8 @@
 import {SessionProvider} from "next-auth/react"
 import React from "react"
 
-const SessionWrapperr = ({children} : {children : React.ReactNode})=>{
+const SessionWrapper = ({children} : {children : React.ReactNode})=>{
   return <SessionProvider>{children}</SessionProvider>
 } 
 
-export default SessionWrapperr 
+export default SessionWrapper
