1f3194cb791005c9d4532284061d181aae410ac5
chnihel <cherif.nihel456@gmail.com>
2025-01-15 13:46:35 +0100

    second commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,12 @@ import "./globals.css";
 import SessionWrapper from "./components/SessionWrapper";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "./fonts/GeistVF.wof",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "./fonts/GeistMonoVF.wof",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
