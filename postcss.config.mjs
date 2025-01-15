79d992fcfe21e59a3d2d18e3e23b3b76dc86fbad
chnihel <cherif.nihel456@gmail.com>
2025-01-15 13:56:41 +0100

    second commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
+const configg = {
   plugins: {
     tailwindcss: {},
   },
 };
 
-export default config;
+export default configg;
